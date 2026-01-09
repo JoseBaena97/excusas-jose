83c1eefab53b20480ed00794b16d18a3f459a07b
JoseBaena97 <josembaena97@gmail.com>
2026-01-09 22:08:50 +0100

    update script
    Add a function to generate random excuses

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-import "bootstrap";
-import "./style.css";
+document.addEventListener('DOMContentLoaded', generadorexcusas);
 
+function generadorexcusas() {
+  let who = ['Mi gato', 'Un ninja', 'El repartidor de pizza', 'Un alien', 'Mi hermano pequeño'];
+  let action = ['robó', 'escondió', 'tiró', 'pintó', 'se comió'];
+  let what = ['mis tareas', 'mi ordenador', 'mi bocata', 'mis zapatillas', 'mi mochila'];
+  let when = ['durante el examen', 'mientras me duchaba', 'a medianoche', 'mientras corría', 'en mitad de la clase'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  //Seleccion de elementos aleatoria para formar la frase
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  //Construcción de la excusa completa
+  let excusa = `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+
+  document.getElementById("excusa").innerHTML = excusa;
+}
